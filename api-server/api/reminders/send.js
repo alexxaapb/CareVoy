5d6c3d63c842868ff6bc9220bfd1426d105f9f51
alexxaapb <apbelizaire@gmail.com>
2026-06-09 20:14:41 +0000

    fix: use correct SUPABASE_SERVICE_ROLE_KEY env var in all API endpoints

--- api-server/api/reminders/send.js
+++ api-server/api/reminders/send.js
@@ -3,7 +3,7 @@ import { Resend } from 'resend';
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_KEY
+  process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
 export default async function handler(req, res) {
